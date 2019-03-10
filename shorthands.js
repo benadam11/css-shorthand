c5e864c5f791823c97c2b7efb39e23f41e3cd0f9
benadam11 <benjamin.mark.adam@gmail.com>
2019-03-10 12:03:12 -0700

    Add object and JSON versions

---
+++ shorthands.js
@@ -0,0 +1,179 @@
+export const shorthands = {
+  animation: {
+    animationName: "none",
+    animationDuration: "0s",
+    animationTimingFunction: "ease",
+    animationDelay: "0s",
+    animationIterationCount: "1",
+    animationDirection: "normal",
+    animationFillMode: "none",
+    animationPlayState: "running"
+  },
+  background: {
+    backgroundImage: "none",
+    backgroundPosition: "0% 0%",
+    backgroundSize: "auto auto",
+    backgroundRepeat: "repeat",
+    backgroundOrigin: "padding-box",
+    backgroundClip: "border-box",
+    backgroundAttachment: "scroll",
+    backgroundColor: "transparent"
+  },
+  border: {
+    borderWidth: {
+      borderTopWidth: "medium",
+      borderRightWidth: "medium",
+      borderBottomWidth: "medium",
+      borderLeftWidth: "medium"
+    },
+    borderStyle: {
+      borderTopStyle: "none",
+      borderRightStyle: "none",
+      borderBottomStyle: "none",
+      borderLeftStyle: "none"
+    },
+    borderColor: {
+      borderTopColor: "currentcolor",
+      borderRightColor: "currentcolor",
+      borderBottomColor: "currentcolor",
+      borderLeftColor: "currentcolor"
+    }
+  },
+  borderBottom: {
+    borderBottomWidth: "medium",
+    borderBottomStyle: "none",
+    borderBottomColor: "currentcolor"
+  },
+  borderColor: {
+    borderTopColor: "currentcolor",
+    borderRightColor: "currentcolor",
+    borderBottomColor: "currentcolor",
+    borderLeftColor: "currentcolor"
+  },
+  borderLeft: {
+    borderLeftWidth: "medium",
+    borderLeftStyle: "none",
+    borderLeftColor: "currentcolor"
+  },
+  borderRadius: {
+    borderTopLeftRadius: 0,
+    borderTopRightRadius: 0,
+    borderBottomRightRadius: 0,
+    borderBottomLeftRadius: 0
+  },
+  borderRight: {
+    borderRightWidth: "medium",
+    borderRightStyle: "none",
+    borderRightColor: "currentcolor"
+  },
+  borderStyle: {
+    borderTopStyle: "none",
+    borderRightStyle: "none",
+    borderBottomStyle: "none",
+    borderLeftStyle: "none"
+  },
+  borderWidth: {
+    borderTopWidth: "medium",
+    borderRightWidth: "medium",
+    borderBottomWidth: "medium",
+    borderLeftWidth: "medium"
+  },
+  columnRule: {
+    columnRuleWidth: "medium",
+    columnRuleStyle: "none",
+    columnRuleColor: "currentcolor"
+  },
+  columns: {
+    columnWidth: "auto",
+    columnCount: "auto"
+  },
+  flex: {
+    flexGrow: 0,
+    flexShrink: 1,
+    flexBasis: "auto"
+  },
+  flexFlow: {
+    flexDirection: "row",
+    flexWrap: "nowrap"
+  },
+  font: {
+    fontStyle: "normal",
+    fontVariant: "normal",
+    fontWeight: "normal",
+    fontStretch: "normal",
+    fontSize: "medium",
+    lineHeight: "normal",
+    fontFamily: "inherit"
+  },
+  grid: {
+    gridTemplateRows: "none",
+    gridTemplateColumns: "none",
+    gridTemplateAreas: "none",
+    gridAutoRows: "auto",
+    gridAutoColumns: "auto",
+    gridAutoFlow: "row",
+    gridColumnGap: 0,
+    gridRowGap: 0,
+    columnGap: "normal",
+    rowGap: "normal"
+  },
+  gridArea: {
+    gridRowStart: "auto",
+    gridColumnStart: "auto",
+    gridRowEnd: "auto",
+    gridColumnEnd: "auto"
+  },
+  gridColumn: {
+    gridColumnStart: "auto",
+    gridColumnEnd: "auto"
+  },
+  gridRow: {
+    gridRowStart: "auto",
+    gridRowEnd: "auto"
+  },
+  gridTemplate: {
+    gridTemplateColumns: "none",
+    gridTemplateRows: "none",
+    gridTemplateAreas: "none"
+  },
+  listStyle: {
+    listStyleType: "disc",
+    listStylePosition: "outside",
+    listStyleImage: "none"
+  },
+  margin: {
+    marginBottom: 0,
+    marginLeft: 0,
+    marginRight: 0,
+    marginTop: 0
+  },
+  offset: {
+    offsetPosition: "auto",
+    offsetPath: "none",
+    offsetDistance: 0,
+    offsetAnchor: "auto",
+    offsetRotate: "auto"
+  },
+  outline: {
+    outlineColor: "currentcolor",
+    outlineStyle: "none",
+    outlineWidth: "medium"
+  },
+  padding: {
+    paddingBottom: 0,
+    paddingLeft: 0,
+    paddingRight: 0,
+    paddingop: 0
+  },
+  textDecoration: {
+    textDecorationColor: "currentcolor",
+    textDecorationStyle: "solid",
+    textDecorationLine: "none"
+  },
+  transition: {
+    transitionDelay: "0s",
+    transitionDuration: "0s",
+    transitionProperty: "all",
+    transitionTimingFunction: "ease"
+  }
+};
